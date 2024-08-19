24b30550de00fd4344a34f82b1513ca92f2625ee
Sævar Þór Pálsson <thorpalsson92@gmail.com>
2024-08-19 21:35:53 +0200

    punk

--- Assets/Scripts 1/DialogueSystem/Runtime/NodeType/ContinueNodeData.cs
+++ Assets/Scripts 1/DialogueSystem/Runtime/NodeType/ContinueNodeData.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Anchry.Dialogue;
-using UnityEditor.Experimental.GraphView;
 
 public class ContinueNodeData : NodeData
 {
